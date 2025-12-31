e759dddf4df552639c9523fa60cc60847ecc9969
Wahyu Anggana <wahyu.anggana@emarsys.com>
2026-01-01 06:42:28 +0700

    feat: active command can now switch accounts if argument provided

--- cmd/active.go
+++ cmd/active.go
@@ -2,21 +2,32 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
+
 	"github.com/k0wl0n/gctx/pkg/manager"
+	"github.com/spf13/cobra"
 )
 
 var activeCmd = &cobra.Command{
-	Use:   "active",
-	Short: "Show currently active account",
+	Use:   "active [account-name]",
+	Short: "Show or set currently active account",
 	Example: `  # Show the currently active account
-  gctx active`,
+  gctx active
+
+  # Switch to 'my-account' (same as gctx switch)
+  gctx active my-account`,
+	Args: cobra.MaximumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		m, err := manager.New()
 		if err != nil {
 			return err
 		}
 
+		// If an argument is provided, behave like switch
+		if len(args) > 0 {
+			return m.SwitchAccount(args[0])
+		}
+
+		// Otherwise, show active account
 		active, err := m.GetActiveAccount()
 		if err != nil {
 			return err
